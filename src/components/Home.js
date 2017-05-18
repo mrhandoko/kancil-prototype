d0d59a78722fd7d68b0d0bffe4763fd83aee9cec
mrhandoko <bambank.handoko@yahoo.com>
2017-05-18 11:35:41 +0700

    styling css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Home = () => (
   <div>
     <Header />
+    <div className='swiper-container'>
+      <div className='swiper-wrapper'>
+        <div className='swiper-slide'>
+          <h1>Kancil is a lending company<small>Something else here</small></h1>
+        </div>
+        <div className='swiper-slide'>Slide 2</div>
+        <div className='swiper-slide'>Slide 3</div>
+      </div>
+      <div className='swiper-pagination' />
+    </div>
+    <Footer />
   </div>
 )
 
--- src/components/PhoneSelection.js
+++ src/components/PhoneSelection.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
+import Header from '../components/Header'
+
 const PhoneSelection = () => (
   <div>
+    <Header />
     <h1>Phone Selection</h1>
   </div>
 )
