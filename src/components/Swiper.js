52982a9d13853063e90b14aa92e04eed3587d7f7
Handoko <handoko@Handokos-MacBook-Pro.local>
2017-05-23 11:39:56 +0700

    adding component did mount to swiper.js

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -1,18 +1,31 @@
 import React from 'react'
 
-const Swiper = () => (
-  <div>
-    <div className="swiper-container">
-      <div className="swiper-wrapper">
-        <div className="swiper-slide">
-          <h1>Kancil is a lending company<small>Something else here</small></h1>
-        </div>
-        <div className="swiper-slide">Slide 2</div>
-        <div className="swiper-slide">Slide 3</div>
-      </div>
-      <div className="swiper-pagination" />
-    </div>
-  </div>
-)
+class Swiper extends React.Component {
+	componentDidMount() {
+		this.swiper = new Swiper('.swiper-container', {
+			pagination: '.swiper-pagination',
+			paginationClickable: true,
+			autoHeight: true
+		})
+	}
+	render() {
+		return (
+			<div>
+				<div className="swiper-container" ref="swiper">
+					<div className="swiper-wrapper">
+						<div className="swiper-slide">
+							<h1>
+								Kancil is a lending company<small>Something else here</small>
+							</h1>
+						</div>
+						<div className="swiper-slide">Slide 2</div>
+						<div className="swiper-slide">Slide 3</div>
+					</div>
+					<div className="swiper-pagination" />
+				</div>
+			</div>
+		)
+	}
+}
 
 export default Swiper
