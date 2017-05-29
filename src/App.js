9e4cdefb417cad30d822b8fe7f6758c1aabdd24b
mrhandoko <bambank.handoko@yahoo.com>
2017-05-29 11:17:30 +0700

    adding app.js file

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import store from './store';
+import Home from './components/Home';
+import PhoneSelection from './components/PhoneSelection';
+import Login from './components/Login';
+import LoanApplication from './components/LoanApplication';
+import SignUp from './components/SignUp';
+import Thankyou from './components/Thankyou';
+import Status from './components/Status';
+import FAQ from './components/FAQ';
+import UberHome1 from './components/uber/UberHome1';
+import UberHome2 from './components/uber/UberHome2';
+import UberHome3 from './components/uber/UberHome3';
+import UberHome4 from './components/uber/UberHome4';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/phone" component={PhoneSelection} />
+            <Route path="/login" component={Login} />
+            <Route path="/loan-application" component={LoanApplication} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/thankyou" component={Thankyou} />
+            <Route path="/status" component={Status} />
+            <Route path="/faq" component={FAQ} />
+            <Route path="/uber1" component={UberHome1} />
+            <Route path="/uber2" component={UberHome2} />
+            <Route path="/uber3" component={UberHome3} />
+            <Route path="/uber4" component={UberHome4} />
+          </div>
+        </Router>
+      </Provider>
+    )
+  }
+}
+
+export default App
