e14cb01ca3ba5c1ed0823141bda6b40ac6dccc77
mrhandoko <bambank.handoko@yahoo.com>
2017-05-29 15:06:01 +0700

    adding loan review

---
+++ src/components/LoanReview.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Redirect, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Header from './Header';
+import Footer from './Footer';
+
+class LoanReview extends Component {
+  componentDidMount() {
+    console.log(JSON.stringify(this.props.product));
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container" style={{ margin: '25px 0' }}>
+          <div className="row">
+            <div className="col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+              <div className="panel-top">
+                <h4 className="fnt-blue">Loan Review</h4>
+              </div>
+              <div className="panel-bottom">
+                <div className="clean-form">
+                  <h5 className="fnt-grey">Produk</h5>
+                  <div className="form-spacer" />
+                  <h5 className="fnt-grey">Cicilan/Bulan</h5>
+                  <div className="row" style={{ borderTop: '1px solid #eaeaea', margin: '1rem 0', paddingTop: '1rem' }}>
+                    <div className="col-sm-12 col-md-12 col-lg-12">
+                      <button className="tertiary input-full" onClick={event => this.clickLogin(event)}>Kirim Aplikasi Pinjaman</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  product: state.product,
+})
+
+export default connect(mapStateToProps,null)(LoanReview)
--- src/components/Login.js
+++ src/components/Login.js
@@ -50,41 +50,41 @@ class Login extends Component {
       return (
 				<div>
 					<Header />
-        <div className="container" style={{ margin: '25px 0' }}>
-          <div className="row">
-            <div className="col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
-              <div className="panel-top">
-                <h4 className="fnt-blue">Login</h4>
-              </div>
-              <div className="panel-bottom">
-                <div className="clean-form">
-                  <h5 className="fnt-grey">Email</h5>
-                  <input type="text" name className="input-full" onChange={event => this.setUsernameField(event)} />
-                  <div className="form-spacer" />
-                  <h5 className="fnt-grey">Password</h5>
-                  <input type="password" className="input-full"  onChange={event => this.setPasswordField(event)} />
-                  <div className="row" style={{ borderTop: '1px solid #eaeaea', margin: '1rem 0', paddingTop: '1rem' }}>
-                    <div className="col-sm-12 col-md-12 col-lg-12">
-                      <button className="tertiary input-full" onClick={event => this.clickLogin(event)}>Login</button>
-                      { this.state.isSucceed === false && <span style={{ color: 'red' }}>Anda tidak bisa melakukan login. Silakan daftar/Sign Up terlebih dahulu</span>}
+          <div className="container" style={{ margin: '25px 0' }}>
+            <div className="row">
+              <div className="col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+                <div className="panel-top">
+                  <h4 className="fnt-blue">Login</h4>
+                </div>
+                <div className="panel-bottom">
+                  <div className="clean-form">
+                    <h5 className="fnt-grey">Email</h5>
+                    <input type="text" name className="input-full" onChange={event => this.setUsernameField(event)} />
+                    <div className="form-spacer" />
+                    <h5 className="fnt-grey">Password</h5>
+                    <input type="password" className="input-full"  onChange={event => this.setPasswordField(event)} />
+                    <div className="row" style={{ borderTop: '1px solid #eaeaea', margin: '1rem 0', paddingTop: '1rem' }}>
+                      <div className="col-sm-12 col-md-12 col-lg-12">
+                        <button className="tertiary input-full" onClick={event => this.clickLogin(event)}>Login</button>
+                        { this.state.isSucceed === false && <span style={{ color: 'red' }}>Anda tidak bisa melakukan login. Silakan daftar/Sign Up terlebih dahulu</span>}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="row" style={{ borderTop: '1px solid #eaeaea', padding: '1rem', backgroundColor: '#eee' }}>
-                  <div className="col-sm-12 col-md-12 col-lg-12 text-center">
-                    <Link className="forgot-password fnt-sz-s1" to='/signup'>Sign Up</Link>
+                  <div className="row" style={{ borderTop: '1px solid #eaeaea', padding: '1rem', backgroundColor: '#eee' }}>
+                    <div className="col-sm-12 col-md-12 col-lg-12 text-center">
+                      <Link className="forgot-password fnt-sz-s1" to='/signup'>Sign Up</Link>
+                    </div>
+                    {/* <div className="col-sm-9 col-md-6 col-lg-6 text-right">
+                      <a href className="forgot-password fnt-sz-s1">
+                        Forgot your Password?
+                      </a>
+                    </div> */}
                   </div>
-                  {/* <div className="col-sm-9 col-md-6 col-lg-6 text-right">
-                    <a href className="forgot-password fnt-sz-s1">
-                      Forgot your Password?
-                    </a>
-                  </div> */}
                 </div>
               </div>
             </div>
           </div>
-        </div>
-			<Footer />
+			  <Footer />
 			</div>
       )
     }
