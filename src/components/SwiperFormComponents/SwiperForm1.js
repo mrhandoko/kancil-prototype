2706a8c38db7a260939907dc29b2677a28523b1f
mrhandoko <bambank.handoko@yahoo.com>
2017-06-07 14:33:30 +0700

    remove suck datepicker

--- src/components/SwiperFormComponents/SwiperForm1.js
+++ src/components/SwiperFormComponents/SwiperForm1.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import { DateField, DatePicker } from 'react-date-picker';
+import 'react-date-picker/index.css';
 
 const SwiperForm1 = props => (
   <div className="swiper-slide swiper-slide-form">
