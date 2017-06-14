e03be4c6b67580497ff7f0eb9c9e3af926cf412b
mrhandoko <bambank.handoko@yahoo.com>
2017-06-14 15:21:54 +0700

    cihuy

--- src/components/LenddoPage.js
+++ src/components/LenddoPage.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 class LenddoPage extends Component {
   constructor() {
+    super();
+    this.state = {
+      application_id: Math.floor(Math.random() * 999999),
+    }
+  }
+  componentDidMount() {
     // Place this in the constructor where it's run once.
     (function() {
       var la = document.createElement('script');
@@ -11,11 +17,12 @@ class LenddoPage extends Component {
       var s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(la, s);
     })();
+
     // LenddoVerifyConfig was not available in the scope in which the button could read it
     // as a result the button couldn't be configured
     window.LenddoVerifyConfig = {
       scriptId: '59230c27f7a57939fa653526',
-      application_id: 'testing',
+      application_id: this.state.application_id,
       onSubmit: function(cb) {
         var errors = false;
         if (errors === false) {
@@ -23,11 +30,13 @@ class LenddoPage extends Component {
         }
       }
     };
-    super();
+    console.log(this.state.application_id);
   }
   render() {
     return (
-      <div id="lenddo_verify"></div>
+      <div>
+        <div id="lenddo_verify"></div>
+      </div>
     );
   };
 }
